0b460c5c7a87942f9df60474bec366ec98b40e81
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-31 12:33:50 -0700

    Set up file structure for posts, create SQL table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
 
 CREATE TABLE github_users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,3 +9,9 @@ CREATE TABLE github_users (
   email TEXT,
   avatar TEXT
 );
+
+CREATE TABLE posts (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  text TEXT NOT NULL,
+  username TEXT REFERENCES github_users(username)
+)
