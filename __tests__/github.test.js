da56cc00ef4718c78b1ccd7fbb925d3c9c49c8c2
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-31 11:18:31 -0700

    Extract github controller functions to own utils file

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-// jest.mock('../lib/utils/github');
+jest.mock('../lib/utils/github');
 
 describe('gitty routes', () => {
   beforeEach(() => {
@@ -20,5 +20,21 @@ describe('gitty routes', () => {
     expect(req.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+  it('logging in should redirect users to TBD', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(req.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
---
+++ public/js/app.js
@@ -0,0 +1,53 @@
+fetch('/api/v1/auth/verify')
+  .then((res) => {
+    if (res.ok) return res.json();
+    else throw new Error('Not logged in');
+  })
+  .then((user) => {
+    // is logged in
+    renderIsLoggedIn(user);
+  })
+  .catch(() => {
+    // not logged in
+    const button = document.createElement('button');
+    button.textContent = 'Login with GitHub';
+    button.addEventListener('click', () => {
+      window.location.assign('/api/v1/github/login');
+    });
+
+    document.getElementById('root').appendChild(button);
+  });
+
+function renderIsLoggedIn(user) {
+  const root = document.getElementById('root');
+  const p = document.createElement('p');
+  p.textContent = user.username;
+  root.appendChild(p);
+
+  const form = document.createElement('form');
+  const textarea = document.createElement('textarea');
+  textarea.name = 'text';
+
+  const button = document.createElement('button');
+  button.textContent = 'Create Tweet';
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const text = fd.get('text');
+
+    fetch('/api/v1/tweets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text, username: 'test_user' }),
+    });
+  });
+
+  form.appendChild(textarea);
+  form.appendChild(button);
+
+  root.appendChild(form);
+}
