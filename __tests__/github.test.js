bee76d0356a62f9bc478e5a26c9a7211a7234265
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-03-31 10:06:24 -0700

    Logging in redirects to github oauth page

---
+++ __tests__/github.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+// jest.mock('../lib/utils/github');
+
+describe('gitty routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('logging in should redirect to the github oauth page', async () => {
+    const req = await request(app).get('/api/v1/github/login');
+
+    expect(req.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
+  });
+});
